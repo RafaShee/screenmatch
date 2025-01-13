66af641a37c7992c312c5bbf39959ee74b78c7c7
Rafaela <rafaela@sentry.com.br>
2025-01-13 17:56:30 -0300

    atualizacao

---
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -0,0 +1,24 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+import java.util.OptionalDouble;
+
+public class Serie {
+    private String titulo;
+    private Integer totalTemporadas;
+    private double avaliacao;
+    private Categoria genero;
+    private String atores;
+    private String poster;
+    private String sinopse;
+
+    public Serie(DadosSerie dadosSerie){
+        this.titulo = dadosSerie.titulo();
+        this.totalTemporadas = dadosSerie.totalTemporadas();
+        this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
+        this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
+
+    }
+
+}
