44d4e6be6c378041bc35dee18c0e60087972ba8c
Rafaela Delgado <rafaelad054@gmail.com>
2025-03-02 10:08:35 -0300

    mapeando relacionamentos

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -25,7 +25,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @Transient
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios =  new ArrayList<>();
 
     public Serie(DadosSerie dadosSerie){
@@ -39,6 +39,15 @@ public class Serie {
     }
 
     public Serie() {}
+
+    public List<Episodio> getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
+        this.episodios = episodios;
+    }
 
     public Long getId() {
         return id;
@@ -112,6 +121,7 @@ public class Serie {
                 ", avaliacao=" + avaliacao +
                 ", atores='" + atores + '\'' +
                 ", poster='" + poster + '\'' +
-                ", sinopse='" + sinopse + '\'';
+                ", sinopse='" + sinopse + '\''+
+                ", episodios=" + episodios+ '\'';
     }
 }
